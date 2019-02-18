20b56fb3f94f112a6697d30e3d73bdfafa3d229f
Kamal Mukherjee <kamalnyc17@gmail.com>
2019-02-18 10:37:10 -0500

    GIPHY-API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 var animals = ["cat", "dog", "falcon", "horse", "tiger", "crow", "deer", "fox", "pegion", "duck"];
 
-//function to create animal buttons
+// function to create animal buttons
 function createButtons() {
     $(".button-area").empty();
 
@@ -10,9 +10,20 @@ function createButtons() {
       button.text(animals[i]);
       $(".button-area").append(button);
     }
-
   }
 
+// adding new animal as the user submits new names
+$("#add-animal").on("click", function(event) {
+    event.preventDefault();
+
+    // Write code to grab the text the user types into the input field
+    var userInput = $("#animal-input").val().trim();
+    animals.push(userInput);
+    $("#animal-input").val("");
+
+    // calling the function to RE-create animal buttons
+    createButtons();
+  });
 
 // creates the default buttons in the beginning
 createButtons();
